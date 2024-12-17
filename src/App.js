249857904bfd2d04432a792d5cc48f647be8c72c
priyanka24081998 <priyankagoti.istosyinfotech@gmail.com>
2024-12-17 11:50:17 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header1 from './Header1';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   <>
+      <Header1></Header1>
+   </>
+  );
+}
+
+export default App;
